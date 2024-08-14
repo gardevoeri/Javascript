ec66f4cb4d68b62bf2f57a073735fecfc20ba860
gardevoeri <je.santos.silva@gmail.com>
2024-08-14 14:51:30 -0300

    if statements and switch cases

---
+++ Personal Projects/lessons/control-flow.js
@@ -0,0 +1,30 @@
+// IF Statements
+
+// let hour = 15;
+// if (hour >= 6 && hour <= 12) {
+//     console.log('Serving Breakfast');
+// } else if (hour > 12 && hour <= 14) {
+//     console.log('Serving Lunch');
+// } else {
+//     console.log('Serving Dinner');
+// }
+
+// If there's only one line of code inside the IF Statement the {}  are not necessary
+
+// Switch Case Statement
+
+let job = "Sales"
+
+switch (job) {
+    case 'Software Developer':
+        console.log('Writes Code');
+        break;
+    case 'Designer':
+        console.log('Makes User Interface');
+        break;
+    case 'Cloud Engineer':
+        console.log('Manages and Deploy Resources')
+        break;
+    default: 
+        console.log('Works With Customers');
+}
