e2460eb5f9161b308e01561d6368226eac7ab080
gardevoeri <je.santos.silva@gmail.com>
2024-08-14 14:19:50 -0300

    operators - ternary and equality

---
+++ Personal Projects/lessons/operators.js
@@ -0,0 +1,21 @@
+let a = 2;
+let b = '2';
+
+// Loose Equality
+// console.log(a == b); // Compares different data types as truthy
+
+// Strict Equality
+// console.log(a === b); // Compares only the same data type as true
+
+// Ternary Operator
+
+let age = 16;
+const canDrive = age >= 16 ? true: false;
+// console.log(canDrive);
+
+let points = 110;
+const customerType = points > 100 ? 'gold': 'silver';
+// funciona quase como um if statement de uma linha só pra associar valores pre-determinados a uma variável
+console.log(customerType);
+
+
