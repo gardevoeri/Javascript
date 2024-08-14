13cf315cea9be9f40adcfa20195be4474ffeb3f5
gardevoeri <je.santos.silva@gmail.com>
2024-08-14 13:28:45 -0300

    declaring strings

--- Personal Projects/lessons/variables.js
+++ Personal Projects/lessons/variables.js
@@ -1,12 +1,20 @@
-let firstName = 'Jessica';
+// Declaring a variable
 
-console.log(firstName);
+let firstName = 'Jessica';
+// Variables use camelCase in Javascript as convention
+
+// Declaring a Constant
 
 const christmas_2024 = '25/12/2024';
+const COLOR_GREEN = 'green';
+// Constants in Javascript use as a convention UpperCase and snakeCase
 
-// Data Types 
-// INT 
-// STRING 
+// Declaring Strings -> use '', "" and ``
+
+let favoriteFruit = 'strawberries';
+let favoriteIceCream = "chocolate";
+let favoriteProgrammingLanguage = `Javascript`
+
 
 
 
