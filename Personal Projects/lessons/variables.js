edd2cef855a86528ff193756eb7cba8fa4f33a49
gardevoeri <je.santos.silva@gmail.com>
2024-08-14 13:39:13 -0300

    other types of variables

--- Personal Projects/lessons/variables.js
+++ Personal Projects/lessons/variables.js
@@ -15,6 +15,39 @@ let favoriteFruit = 'strawberries';
 let favoriteIceCream = "chocolate";
 let favoriteProgrammingLanguage = `Javascript`
 
+// Declaring Numbers
 
+// int
+let numberOfDonuts = 12;
+//float
+let pi = 3.14;
+//big int use a 'n' at the end of number
+let veryLargeNumber = 54354343363466n; 
+
+
+// Declaring Booleans
+
+let lovesCoding = true;
+let hatesCoding = false;
+
+// Declaring indefined variables
+
+let favoritColor; // without the assignment the variable doesn't have a type
+console.log(favoritColor);
+
+// Declaring nulls -> use to clean a value from a variable
+
+favoriteFruit = null; 
+
+// Declaring a symbol
+
+const uniqueKey = Symbol(); //verify usages
+
+//Declaring objects
+
+let course = {
+    name: 'Javascript for Beginners',
+    hours: 3
+};
 
 
