daa1f3f8a90bb4d01658b500baa7b8e78fd7a0f9
gardevoeri <je.santos.silva@gmail.com>
2024-08-14 15:11:26 -0300

    loops

--- Personal Projects/lessons/control-flow.js
+++ Personal Projects/lessons/control-flow.js
@@ -15,16 +15,49 @@
 
 let job = "Sales"
 
-switch (job) {
-    case 'Software Developer':
-        console.log('Writes Code');
-        break;
-    case 'Designer':
-        console.log('Makes User Interface');
-        break;
-    case 'Cloud Engineer':
-        console.log('Manages and Deploy Resources')
-        break;
-    default: 
-        console.log('Works With Customers');
-}
+// switch (job) {
+//     case 'Software Developer':
+//         console.log('Writes Code');
+//         break;
+//     case 'Designer':
+//         console.log('Makes User Interface');
+//         break;
+//     case 'Cloud Engineer':
+//         console.log('Manages and Deploy Resources')
+//         break;
+//     default: 
+//         console.log('Works With Customers');
+// }
+
+// FOR LOOPS
+
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+
+// WHILE LOOPS
+
+// let i = 0; 
+// while (i < numbers.length) {
+//     console.log(numbers[i]);
+//     i++;
+// }
+
+// let sum = 0;
+// while (true) {
+//     console.log('Loop');
+//     sum++;
+
+//     if (sum === 10)
+//         break;
+// }
+
+let i = 0;
+
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
